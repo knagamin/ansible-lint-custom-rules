1dbfb2d1981b79fbdfaecbb6b50a557e23df8e22
Kiyo Nagamine <knagamin.hub@gmail.com>
2020-03-11 00:46:54 +0900

    enhancement: implement config loader class

--- rules/TaskHasInvalidName.py
+++ rules/TaskHasInvalidName.py
@@ -1,32 +1,26 @@
-from ansiblelint import AnsibleLintRule
-from rules.lib.utils import load_custom_rule_options
 import re
+from ansiblelint import AnsibleLintRule
+from rules.lib.utils import RuleConfigLoader
 
 
 class TaskHasInvalidName(AnsibleLintRule):
     # These are Default Values
     id = None
-    description = None
     tags = None
-    opts = None
+    shortdesc = None
+    description = None
 
     def __init__(self):
         # Override variables if options are defined in external file
-        self.opts = load_custom_rule_options(self.__class__.__name__)
-        try:
-            self.id = self.opts['id']
-            self.shortdesc = self.opts['shortdesc']
-            self.description = self.opts['description']
-            self.shortdesc = self.opts['shortdesc']
-            self.tags = self.opts['tags']
-        except KeyError:
-            pass
+        self.rcl = RuleConfigLoader(self.__class__.__name__)
+        self.rcl.load_config()
+        self.rcl.set_metainfo_to_rule(self)
 
     def matchtask(self, file, task):
         return self.has_invalid_name(file, task)
 
     def is_invalid_task_name(self, task_name):
-        return re.match(self.opts['valid_pattern'], task_name) is None
+        return re.match(self.rcl.get_config('valid_pattern'), task_name) is None
 
     def has_invalid_name(self, _file, task):
         if 'name' in task:
--- rules/lib/utils.py
+++ rules/lib/utils.py
@@ -1,15 +1,52 @@
+r""" Load and manage external configuration for Ansible Lint Rules
+"""
+
 import os
 import yaml
 
-CUSTOM_CONFIG_PATH = ".ansible-lint-custom.yml"
+CURDIR = os.path.dirname(__file__)
+CONFIG_PATH = os.path.join(CURDIR, "..", "..", ".ansible-lint-custom.yml")
+DEFAULT_ATTRS = ('id', 'tags', 'shortdesc', 'description')
 
-def load_custom_rule_options(rulename):
-    options = None
-    if os.path.exists(CUSTOM_CONFIG_PATH):
-        with open(CUSTOM_CONFIG_PATH, "r") as file:
+
+def load_custom_rule_config(path):
+    """ Load external configuration file
+    """
+    if os.path.exists(path):
+        with open(path, "r") as file:
             try:
-                options = yaml.safe_load(file)
-                return options[rulename]
+                config = yaml.safe_load(file)
+                return config
             except yaml.YAMLError:
                 pass
     return None
+
+
+class RuleConfigLoader(object):
+    """ Load and manage external configuration for Ansible Lint Rules
+    """
+
+    config = None
+
+    def __init__(self, rulename, path=CONFIG_PATH):
+        self.rulename = rulename
+        self.path = path
+
+    def load_config(self):
+        """ Load configuration of the rule
+        """
+        self.config = load_custom_rule_config(self.path)[self.rulename]
+
+    def set_metainfo_to_rule(self, rule):
+        """ Set meta information (id, tags, shortdesc, description)
+            to a `AnsibleLintRule` object
+        """
+        for attr in DEFAULT_ATTRS:
+            if attr in self.config:
+                setattr(rule, attr, self.config[attr])
+
+    def get_config(self, key=None):
+        """ Get whole configurations or single one
+        :param key: a specific configuration
+        """
+        return self.config[key] if key else self.config
